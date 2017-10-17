dcc9b7b543f6f31bf7b13313bd1bdb4ae8b30656
Josh Willhite <joshua@janrain.com>
2017-10-17 14:48:13 -0700

    update

--- rplugin/python3/nvim-slack.py
+++ rplugin/python3/nvim-slack.py
@@ -10,7 +10,6 @@ class NeoSlack(object):
     def __init__(self, nvim):
         self.nvim = nvim
         self.sc = SlackClient(os.environ['SLACK_TOKEN'])
-        self.summary =
 
     def _get_buffer(self, buffer_name):
         return [b for b in self.nvim.buffers if b.name == buffer_name][0]
@@ -30,5 +29,5 @@ class NeoSlack(object):
         self.nvim.command('new {}'.format(buff_name))
         self.nvim.command('view')
         buff = self._get_buffer(buff_name)
-        for line in json.dumps(self.get_summar(), indent=2).split('\n'):
+        for line in json.dumps(self.get_summary(), indent=2).split('\n'):
             buff.append(line)
