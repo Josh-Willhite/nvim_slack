8441476d24c59becea142b493779e64715b47add
Josh Willhite <joshua@janrain.com>
2017-10-17 07:53:37 -0700

    Test plugin/slack integration.

---
+++ rplugin/python3/nvim-slack.py
@@ -0,0 +1,18 @@
+import json
+import os
+
+import neovim
+from slackclient import SlackClient
+
+@neovim.plugin
+class NeoSlack(object):
+
+    def __init__(self, nvim):
+        self.nvim = nvim
+        self.sc = SlackClient(os.environ['SLACK_TOKEN'])
+
+    @neovim.command("SlackChannels")
+    def slack_channels(self):
+        channels = sc.api_call("channels.list")['channels']
+        channels = [ch['name'] for ch in channels if ch['id'] == channel_id]
+            self.nvim.current.line = json.dumps(channels, indent=2)
