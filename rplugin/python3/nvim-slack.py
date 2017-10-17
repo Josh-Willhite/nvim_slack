66b6122cf80925592bfdf02086e4a10202984489
Josh Willhite <joshua@janrain.com>
2017-10-17 13:21:23 -0700

    syntax

--- rplugin/python3/nvim-slack.py
+++ rplugin/python3/nvim-slack.py
@@ -6,7 +6,6 @@ from slackclient import SlackClient
 
 @neovim.plugin
 class NeoSlack(object):
-
     def __init__(self, nvim):
         self.nvim = nvim
         self.sc = SlackClient(os.environ['SLACK_TOKEN'])
@@ -15,4 +14,4 @@ class NeoSlack(object):
     def slack_channels(self):
         channels = sc.api_call("channels.list")['channels']
         channels = [ch['name'] for ch in channels if ch['id'] == channel_id]
-            self.nvim.current.line = json.dumps(channels, indent=2)
+        self.nvim.current.line = json.dumps(channels, indent=2)
