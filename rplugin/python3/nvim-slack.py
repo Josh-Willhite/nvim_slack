697c3c08ce1ea81f52a86782e77c29e3044ba604
Josh Willhite <joshua@janrain.com>
2017-10-17 14:47:14 -0700

    update

--- rplugin/python3/nvim-slack.py
+++ rplugin/python3/nvim-slack.py
@@ -4,14 +4,25 @@ import os
 import neovim
 from slackclient import SlackClient
 
+
 @neovim.plugin
 class NeoSlack(object):
     def __init__(self, nvim):
         self.nvim = nvim
         self.sc = SlackClient(os.environ['SLACK_TOKEN'])
+        self.summary =
 
     def _get_buffer(self, buffer_name):
         return [b for b in self.nvim.buffers if b.name == buffer_name][0]
+
+    def get_summary(self):
+        channels = self.sc.api_call("channels.list")['channels']
+        users = self.sc.api_call("users.list")["members"]
+        users = {u['id']:u['real_name'] for u in users}
+        return = {
+            channel['name']: {'members':[users[m] for m in channel['members']]}
+            for channel in channels
+        }
 
     @neovim.command("SlackChannels")
     def slack_channels(self):
@@ -19,6 +30,5 @@ class NeoSlack(object):
         self.nvim.command('new {}'.format(buff_name))
         self.nvim.command('view')
         buff = self._get_buffer(buff_name)
-        channels = self.sc.api_call("channels.list")['channels']
-        for channel in channels:
-            buff.append(str(channel))
+        for line in json.dumps(self.get_summar(), indent=2).split('\n'):
+            buff.append(line)
