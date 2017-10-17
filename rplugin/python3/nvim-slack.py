526703fd3b7fd73184b14f4c6ffb13751f56b792
Josh Willhite <joshua@janrain.com>
2017-10-17 13:52:43 -0700

    update

--- rplugin/python3/nvim-slack.py
+++ rplugin/python3/nvim-slack.py
@@ -10,8 +10,16 @@ class NeoSlack(object):
         self.nvim = nvim
         self.sc = SlackClient(os.environ['SLACK_TOKEN'])
 
+    def _get_buffer(self, buffer_name):
+        return [b for b in nvim.buffers if b.name == buffer_name][0]
+
     @neovim.command("SlackChannels")
     def slack_channels(self):
+        nvim.command('new /tmp/slack_channels')
+        nvim.command('view')
+        buff = self._get_buffer('/tmp/slack_channels')
+
         channels = self.sc.api_call("channels.list")['channels']
         channels = [ch['name'] for ch in channels]
-        self.nvim.current.line = str(channels)
+        for channel in channels:
+            buff.append(channel)
